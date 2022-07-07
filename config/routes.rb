ff94d2d44b316863dc9aee0941625013635c351e
Cristobal Dominguez <cristobald@gmail.com>
2022-07-06 22:52:01 -0400

    Adds Challenge Development

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :stories
+  resources :users, only: %i[ new create show ]
+  resources :sessions, only: %i[ new create destroy ]
 
   root 'stories#index'
 end
